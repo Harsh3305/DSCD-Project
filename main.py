3cb02da119131efffff941e3de1b3bdf3dcaf95c
Harsh3305 <harshverma3305@gmail.com>
2023-04-20 17:42:34 +0530

    Minor changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.master.master import Master
 if __name__ == "__main__":
     master = Master(
         input_dir="/home/harsh/Project/DSCD/AProject/input",
-        mapper_addresses=["localhost:8080", "localhost:8081"],
+        mapper_addresses=["localhost:8080", "localhost:808"],
         reducer_addresses=["localhost:3000", "localhost:3001", "localhost:3002"],
         output_dir="/home/harsh/Project/DSCD/AProject/output"
     )
