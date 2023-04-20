e5cd0cb3f29b22c8f9e7f89aa64f3f2a9ca8d196
Harsh3305 <harshverma3305@gmail.com>
2023-04-20 17:40:56 +0530

    Remove reading file and minor changes are done

--- src/master/master.py
+++ src/master/master.py
@@ -1,5 +1,4 @@
 import os
-from src.file_read import reader
 from src.communication.send_data_to_mapper import MapperCommunication
 from src.logging.logging import Logging
 
@@ -20,27 +19,16 @@ class Master(MapperCommunication, Logging):
         self.number_of_reducer = len(reducer_addresses)
         self.output_dir = output_dir
 
-    def process_files(self):
-        self.read_input_file()
-        self.split_input()
-
-    def read_input_file(self):
+    def process_data(self):
         files_in_path = os.listdir(self.input_dir)
-        all_file_content = []
-        for file_path in files_in_path:
-            file_content = reader.read_single_file(file_path, self.input_dir)
-            all_file_content.append(file_content)
-        self.content = all_file_content
-        self.log(f"{len(files_in_path)} files read successfully")
+        for current_file_path in files_in_path:
+            self._send_input_file_address_to_mapper(self.input_dir+"/"+current_file_path)
 
-    def split_input(self):
-        for data in self.content:
-            self.send_split_data_to_mapper(data=data)
+    def _send_input_file_address_to_mapper(self, file_path):
+        address_of_mapper = self.mapper_addresses[self._round_robin_index]
+        self._round_robin_index = (self._round_robin_index + 1) % len(self.mapper_addresses)
 
-    def send_split_data_to_mapper(self, data):
-        mapper_address = self.mapper_addresses[self._round_robin_index]
-        self._round_robin_index = (self._round_robin_index + 1) % self.number_of_mapper
         self.send_data_to_mapper(
-            mapper_address=mapper_address,
-            data=data
+            mapper_address=address_of_mapper,
+            input_file_path=file_path
         )
