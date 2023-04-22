62410f89289f2530e8ba94a53c98d1042f7f8977
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-04-22 14:44:17 +0530

    Make reducer compatible with mapper (#3)
    * Updated proto
    
    * Added input file
    
    * Changes done in reduver to handle its operation
    
    * Change communication layer
    
    * Link updated communication layer with master

--- message_pb2.py
+++ message_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rmessage.proto\x12\x06mapper\">\n\rMapperRequest\x12\x11\n\tfile_name\x18\x01 \x01(\t\x12\x1a\n\x12number_of_reducers\x18\x02 \x01(\x03\"\r\n\x0bMapperReply2H\n\x06Mapper\x12>\n\x10SendFileLocation\x12\x15.mapper.MapperRequest\x1a\x13.mapper.MapperReplyb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rmessage.proto\x12\x06mapper\">\n\rMapperRequest\x12\x11\n\tfile_name\x18\x01 \x01(\t\x12\x1a\n\x12number_of_reducers\x18\x02 \x01(\x03\"\r\n\x0bMapperReply\"+\n\x0eReducerRequest\x12\x19\n\x11\x66ile_name_pattern\x18\x01 \x01(\t\"\x0e\n\x0cReducerReply2H\n\x06Mapper\x12>\n\x10SendFileLocation\x12\x15.mapper.MapperRequest\x1a\x13.mapper.MapperReply2J\n\x07Reducer\x12?\n\x0fSendFilePattern\x12\x16.mapper.ReducerRequest\x1a\x14.mapper.ReducerReplyb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'message_pb2', globals())
@@ -24,6 +24,12 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _MAPPERREQUEST._serialized_end=87
   _MAPPERREPLY._serialized_start=89
   _MAPPERREPLY._serialized_end=102
-  _MAPPER._serialized_start=104
-  _MAPPER._serialized_end=176
+  _REDUCERREQUEST._serialized_start=104
+  _REDUCERREQUEST._serialized_end=147
+  _REDUCERREPLY._serialized_start=149
+  _REDUCERREPLY._serialized_end=163
+  _MAPPER._serialized_start=165
+  _MAPPER._serialized_end=237
+  _REDUCER._serialized_start=239
+  _REDUCER._serialized_end=313
 # @@protoc_insertion_point(module_scope)
--- message_pb2_grpc.py
+++ message_pb2_grpc.py
@@ -65,3 +65,64 @@ class Mapper(object):
             message__pb2.MapperReply.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+
+class ReducerStub(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def __init__(self, channel):
+        """Constructor.
+
+        Args:
+            channel: A grpc.Channel.
+        """
+        self.SendFilePattern = channel.unary_unary(
+                '/mapper.Reducer/SendFilePattern',
+                request_serializer=message__pb2.ReducerRequest.SerializeToString,
+                response_deserializer=message__pb2.ReducerReply.FromString,
+                )
+
+
+class ReducerServicer(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def SendFilePattern(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+
+def add_ReducerServicer_to_server(servicer, server):
+    rpc_method_handlers = {
+            'SendFilePattern': grpc.unary_unary_rpc_method_handler(
+                    servicer.SendFilePattern,
+                    request_deserializer=message__pb2.ReducerRequest.FromString,
+                    response_serializer=message__pb2.ReducerReply.SerializeToString,
+            ),
+    }
+    generic_handler = grpc.method_handlers_generic_handler(
+            'mapper.Reducer', rpc_method_handlers)
+    server.add_generic_rpc_handlers((generic_handler,))
+
+
+ # This class is part of an EXPERIMENTAL API.
+class Reducer(object):
+    """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def SendFilePattern(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/mapper.Reducer/SendFilePattern',
+            message__pb2.ReducerRequest.SerializeToString,
+            message__pb2.ReducerReply.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
